177fd0b7f661d1784aa8036419b08a079f67c532
91898 <chandansatyendraprasad@gmail.com>
2023-04-25 21:01:51 +0530

    Math

---
+++ src/DayFiveMathDemo.java
@@ -0,0 +1,10 @@
+public class DayFiveMathDemo {
+    public static void main(String[] args) {
+        System.out.println("VALUE OF PI = "+Math.PI);
+        System.out.println("MINIMUM= "+Math.min(5,4));
+        System.out.println("MAXIMUM= "+Math.max(5,4));
+        System.out.println("SQUARE ROOT= "+Math.sqrt(64));
+        System.out.println("Absolute = "+Math.abs(64));
+        System.out.println("Random = "+Math.random());
+    }
+}
